a16de4c791949579c2ee876e63c3a9fc7133f672
PandemonikaBomber <88555148+PandemonikaBomber@users.noreply.github.com>
2021-08-06 20:43:25 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pandemonika_Bomber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
